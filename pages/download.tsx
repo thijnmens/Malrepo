284455b76af53fe922a84a5c61b69baf4a3696d7
thijnmens <thijnsmulders04@gmail.com>
2022-02-21 19:16:27 +0100

    download page now has content

--- pages/download.tsx
+++ pages/download.tsx
@@ -1,5 +1,8 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+import useTimer from '../hooks/useTimer';
 
 declare global {
 	namespace JSX {
@@ -9,8 +12,28 @@ declare global {
 	}
 }
 
+const download = (x: number) => {
+	if (x === 0) {
+		return (
+			<Link passHref href={`/download`}>
+				<button className="bg-gradient-to-r from-blue-600 to-purple-700 rounded-xl p-3 hover:bg-gradient-to-l transition-all ease-in-out duration-250 hover:scale-105 translate-y-[45%]">
+					Download
+				</button>
+			</Link>
+		);
+	} else {
+		return (
+			<Link passHref href={`/download`}>
+				<button className="bg-gradient-to-r from-gray-600 to-gray-700 rounded-xl p-3 translate-y-[45%]">
+					<Image src="/Eclipse-1s-200px.svg" layout="fill" />
+				</button>
+			</Link>
+		);
+	}
+};
+
 const Download: NextPage = () => {
-	const time = 0;
+	const time = useTimer(5);
 	return (
 		<div>
 			<Head>
@@ -28,7 +51,15 @@ const Download: NextPage = () => {
 			</Head>
 
 			<main>
-				<div className="h-[100vh]">
+				<div className="m-auto translate-y-[50%] w-[60vw] h-[20vh] bg-gray-800">
+					<div className="m-auto translate-y-[25%] h-full w-[80%] text-center">
+						<h1 className="text-3xl">
+							Your download will be ready in <strong>{time}</strong> seconds
+						</h1>
+						{download(time)}
+					</div>
+				</div>
+				<div className="h-[20vh]">
 					<amp-auto-ads
 						type="adsense"
 						data-ad-client="ca-pub-4713306956805377"
