f7ae06cc906cce7410efe21cf85a92de520cc76c
thijnmens <thijnsmulders04@gmail.com>
2022-02-21 20:22:12 +0100

    moved jerry the cat to a new home

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,25 +1,25 @@
-import styles from "../styles/404.module.css";
-import Image from "next/image";
-import Link from "next/link";
+import styles from '../styles/404.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const NoPage = () => {
 	return (
 		<div className="m-auto w-1/3">
-			  <h1 className={styles.bigText}> 404 </h1>
-			  <h2 className={styles.smallText}>
-			  	You seem to be lost, here Jerry the Cat will take you back{" "}
-			  	<Link href="/">
-			  		<a className={styles.link}>home!</a>
-			  	</Link>
-			  </h2>
+			<h1 className={styles.bigText}> 404 </h1>
+			<h2 className={styles.smallText}>
+				You seem to be lost, here Jerry the Cat will take you back{' '}
+				<Link href="/">
+					<a className={styles.link}>home!</a>
+				</Link>
+			</h2>
 
-			  <Link href="/">
-			  	<a>
-			  		<Image src="/404/jerry the cat.png" alt="" width="200" height="200"></Image>
-			  	</a>
-			  </Link>
+			<Link href="/">
+				<a>
+					<Image src="/404.png" alt="" width="200" height="200"></Image>
+				</a>
+			</Link>
 
-			  <h6 className={styles.hint}>(hint: click the cat)</h6>
+			<h6 className={styles.hint}>(hint: click the cat)</h6>
 		</div>
 	);
 };
